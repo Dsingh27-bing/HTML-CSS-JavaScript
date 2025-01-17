10c0f56a515f68ee7dd6845217fcf5a2b569e083
Dimple Singh <dsingh27@binghamton.edu>
2025-01-17 18:02:55 -0500

    HTML basics commit
    all projects based on HTML

---
+++ HTML/LifeAtAntraKeyboard/index.js
@@ -0,0 +1,39 @@
+function antra(){
+    window.open("https://www.antra.com/", "_blank");
+}
+function google(){
+    window.open("https://www.google.com/", "_blank");
+}
+function github(){
+    window.open("https://www.github.com/", "_blank");
+}
+function facebook(){
+    window.open("https://www.facebook.com/", "_blank");
+}
+function gmail(){
+    window.open("https://www.gmail.com/", "_blank");
+}
+function youtube(){
+    window.open("https://www.youtube.com/", "_blank");
+}
+function google2048(){
+    window.open("https://2048game.com/", "_blank");
+}
+function food(){
+    window.open("https://www.foodnetwork.com/", "_blank");
+}
+function beer(){
+    window.open("https://www.brewersassociation.org/edu/brewers-association-beer-style-guidelines/", "_blank");
+}
+function sleep(){
+    window.open("https://www.sleepfoundation.org/", "_blank");
+}
+function WC(){
+    window.open("https://www.fifa.com/en", "_blank");
+}
+function social(){
+    window.open("https://www.socialmediatoday.com/", "_blank");
+}
+function game(){
+    window.open("https://supermario-game.com/", "_blank");
+}
